57f9fafa9bfa069a195d76f935137a42f280360d
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-05 15:26:02 -0300

    Estilisa layout e exibicao de erros

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,15 +1,26 @@
 import React from "react"
-import * as S from './styled'
+import useGithub from "../../hooks/github.hooks";
+import Header from "../header";
+import * as S from './styled';
+import img from './logo192.png'
 
 const Layout = ({ children }) => {
+    const { githubState } = useGithub();
+
     return (
         <S.wrapperLayout>
-            <section>
-                <header>
-                    Header
-                </header>
-                {children}
-            </section>
+            <Header />
+            {githubState.loading && (<p>Loading....</p>)}
+            {githubState.userNotFound && (<S.msgError><p>Nenhum usuário foi encontrado</p></S.msgError>)}
+            {githubState.hasUser ?
+                <>{children}</>
+                :
+                <>
+                    <S.wrapperWaitingSearch>
+                        <img src={img} alt="Icone React" />
+                        <h4>Aguardando a sua pesquisa...</h4>
+                    </S.wrapperWaitingSearch>
+                </>}
         </S.wrapperLayout>
     )
 
