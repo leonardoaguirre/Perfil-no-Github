59f90142175046a95a46001952a58918dfc5c61e
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-05 12:14:33 -0300

    Implementa o provider do githubContext

---
+++ src/hooks/github.hooks.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import { GitHubContext } from '../providers/github.provider'
+
+const useGithub = () => {
+    const {
+        githubState,
+        getUser,
+        getUserRepos,
+        getUserStarred
+    } = useContext(GitHubContext);
+
+    return {
+        githubState,
+        getUser,
+        getUserRepos,
+        getUserStarred
+    }
+}
+
+export default useGithub;
